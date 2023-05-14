fd74b75339837cd0128ebf10f64e6ba9391e90d9
Walid Hanif Ataullah <walidhnf12@gmail.com>
2023-05-14 16:22:00 +0700

    Melakukan Unit Testing

---
+++ TestProject1/MahasiswaTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TUBES_KPL_NONGUI;
+
+namespace TUBES_KPL_NONGUI_Test
+{
+    [TestFixture]
+    public class MahasiswaTest
+    {
+        [Test]
+        public void TestMahasiswaProperties()
+        {
+            // Arrange
+            string expectedNama = "John Doe";
+            int expectedKehadiran = 5;
+
+            // Act
+            Mahasiswa mahasiswa = new Mahasiswa
+            {
+                Nama = expectedNama,
+                Kehadiran = expectedKehadiran
+            };
+
+            // Assert
+            Assert.AreEqual(expectedNama, mahasiswa.Nama);
+            Assert.AreEqual(expectedKehadiran, mahasiswa.Kehadiran);
+        }
+    }
+}
