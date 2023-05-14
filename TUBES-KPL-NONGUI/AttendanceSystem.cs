fd1d480462ffba6137665e8a8338d514ead6a522
zynexe <zuhayrraffa@gmail.com>
2023-05-14 14:33:28 +0700

    implementasi fiturNotifikasi

---
+++ TUBES-KPL-NONGUI/AttendanceSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUBES_KPL_NONGUI
+{
+    public class AttendanceSystem
+    {
+        private Dictionary<string, bool> attendanceData;
+        private Dictionary<bool, string> attendanceStatus;
+
+        public AttendanceSystem()
+        {
+            attendanceData = new Dictionary<string, bool>();
+            attendanceStatus = new Dictionary<bool, string> {
+            { true, "Hadir" },
+            { false, "absen" }
+        };
+        }
+
+        public void MarkAttendance(string studentId, bool isPresent)
+        {
+            attendanceData[studentId] = isPresent;
+        }
+
+        public void AlertAbsentStudents()
+        {
+            foreach (KeyValuePair<string, bool> studentAttendance in attendanceData)
+            {
+                string studentId = studentAttendance.Key;
+                bool isPresent = studentAttendance.Value;
+                string attendanceStatusMsg = attendanceStatus[isPresent];
+                Console.WriteLine("Notifikasi Absen {0}: anda {1} dari kelas hari ini.", studentId, attendanceStatusMsg);
+                // You could also send an email or text message to the student here
+            }
+        }
+    }
+}
--- TUBES-KPL-NONGUI/Program.cs
+++ TUBES-KPL-NONGUI/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TUBES_KPL_NONGUI
 {
@@ -10,6 +7,17 @@ namespace TUBES_KPL_NONGUI
     {
         static void Main(string[] args)
         {
+            AttendanceSystem attendanceSystem = new AttendanceSystem();
+            attendanceSystem.MarkAttendance("student1", true); // student1 is present
+            attendanceSystem.MarkAttendance("student2", false); // student2 is absent
+            attendanceSystem.MarkAttendance("student3", true); // student3 is present
+
+            attendanceSystem.AlertAbsentStudents();
+            Console.WriteLine("tes");
+
         }
     }
+
+   
+
 }
