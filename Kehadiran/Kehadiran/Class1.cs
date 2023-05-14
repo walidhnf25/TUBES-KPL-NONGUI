b6217ac7c5f0cea34b3af74140dfab045c90eada
Walid Hanif Ataullah <walidhnf12@gmail.com>
2023-05-14 14:55:16 +0700

    Menambahkan Library Presentase Kehadiran

---
+++ Kehadiran/Kehadiran/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace KehadiranLibrary
+{
+    public class Kehadiran
+    {
+        public static double HitungPresentaseKehadiran(List<Mahasiswa> daftarMahasiswa)
+        {
+            double totalKehadiran = 0;
+            foreach (Mahasiswa mhs in daftarMahasiswa)
+            {
+                totalKehadiran += mhs.Kehadiran;
+            }
+            double presentaseKehadiran = totalKehadiran / (daftarMahasiswa.Count * 16) * 100;
+            return presentaseKehadiran;
+        }
+    }
+
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public int Kehadiran { get; set; }
+    }
+}
